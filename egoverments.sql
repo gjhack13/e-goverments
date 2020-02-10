cba76c98575fdd07f719199e8eaea3472ea887bc
zakkyfirdaus <zakky188@gmail.com>
2020-02-10 20:32:28 +0700

    Membuat file SQL untuk database

---
+++ egoverments.sql
@@ -0,0 +1,121 @@
+-- phpMyAdmin SQL Dump
+-- version 4.6.5.2
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: 10 Feb 2020 pada 14.23
+-- Versi Server: 10.1.21-MariaDB
+-- PHP Version: 5.6.30
+
+SET SQL_MODE
+= "NO_AUTO_VALUE_ON_ZERO";
+SET time_zone
+= "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `egoverments`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `user`
+--
+
+CREATE TABLE `user`
+(
+  `id` int
+(11) NOT NULL,
+  `name` varchar
+(123) NOT NULL,
+  `email` varchar
+(123) NOT NULL,
+  `image` varchar
+(123) NOT NULL,
+  `password` varchar
+(234) NOT NULL,
+  `role_id` int
+(11) NOT NULL,
+  `is_active` int
+(1) NOT NULL,
+  `date_created` int
+(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data untuk tabel `user`
+--
+
+INSERT INTO `user`
+  (`id`, `name
+`, `email`, `image`, `password`, `role_id`, `is_active`, `date_created`) VALUES
+(1, 'Zakky Firdaus', 'zakkyfirdaus97@yahoo.co.id', 'default.jpg', '$2y$10$F0nJ5oePPm.JbGrFQIPYne7kJ3/5VnTUNrnjn0hCLnalkG6lbHQKK', 2, 1, 1581340814);
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `user_role`
+--
+
+CREATE TABLE `user_role`
+(
+  `id` int
+(11) NOT NULL,
+  `role` varchar
+(123) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data untuk tabel `user_role`
+--
+
+INSERT INTO `user_role` (`
+id`,
+`role
+`) VALUES
+(1, 'Administrator'),
+(2, 'Member');
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `user`
+--
+ALTER TABLE `user`
+ADD PRIMARY KEY
+(`id`);
+
+--
+-- Indexes for table `user_role`
+--
+ALTER TABLE `user_role`
+ADD PRIMARY KEY
+(`id`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `user`
+--
+ALTER TABLE `user`
+  MODIFY `id` int
+(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+--
+-- AUTO_INCREMENT for table `user_role`
+--
+ALTER TABLE `user_role`
+  MODIFY `id` int
+(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
