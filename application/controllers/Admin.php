0e1a907123b338fb7b325bc6fa340d3637970238
zakkyfirdaus <zakky188@gmail.com>
2020-02-14 06:55:39 +0700

    Membuat fungsi menu pada sistem

--- application/controllers/Admin.php
+++ application/controllers/Admin.php
@@ -7,7 +7,7 @@ class Admin extends CI_Controller
 
     public function index()
     {
-        $data['title'] = 'Dashboard Admin';
+        $data['title'] = 'Dashboard';
         $data['user'] = $this->db->get_where('user', ['email' => $this->session->userdata('email')])->row_array();
         $this->load->view('templates/header', $data);
         $this->load->view('templates/sidebar', $data);
---
+++ application/controllers/Menu.php
@@ -0,0 +1,31 @@
+<?php
+defined('BASEPATH') or exit('No direct script access allowed');
+
+class Menu extends CI_Controller
+{
+
+
+    public function index()
+    {
+        $data['title'] = 'Menu Manage';
+        $data['user'] = $this->db->get_where('user', ['email' => $this->session->userdata('email')])->row_array();
+
+        $data['menu'] = $this->db->get('user_menu')->result_array();
+
+        $this->form_validation->set_rules('menu', 'Menu', 'required');
+
+        if ($this->form_validation->run() == false) {
+
+            $this->load->view('templates/header', $data);
+            $this->load->view('templates/sidebar', $data);
+            $this->load->view('templates/topbar', $data);
+            $this->load->view('menu/index', $data);
+            $this->load->view('templates/footer');
+        } else {
+            $this->db->insert('user_menu', ['menu' => $this->input->post('menu')]);
+            $this->session->set_flashdata('message', '<div class="alert alert-success" role="alert">
+                    Menu berhasil ditambahkan!</div>');
+            redirect('menu');
+        }
+    }
+}
