8ea61638f5738ad71c205f0842b0e8a2182538d1
zakkyf <zakkyfirdaus@icloud.com>
2020-02-15 02:01:55 +0700

    Perubahan pada auth login

--- application/views/auth/login.php
+++ application/views/auth/login.php
@@ -16,16 +16,19 @@
                                     <h1 class="h4 text-gray-900 mb-4">Selamat Datang!</h1>
                                 </div>
 
-                                <?= $this->session->flashdata('message'); //pesan untuk akun yang telah berhasil terdaftar ?>
+                                <?= $this->session->flashdata('message'); //pesan untuk akun yang telah berhasil terdaftar 
+                                ?>
 
                                 <form class="user" method="POST" action="<?= base_url('auth'); ?>">
                                     <div class="form-group">
-                                        <input type="text" class="form-control form-control-user" id="email" name="email" placeholder="Enter Email Address..." value="<?= set_value('email')?>">
-                                        <?= form_error('email','<small class="text-danger pl-3">','</small>'); //menampilkan pemberitahuan error saat tidak memasukkan apapun keform ?>
+                                        <input type="text" class="form-control form-control-user" id="email" name="email" placeholder="Enter Email Address..." value="<?= set_value('email') ?>">
+                                        <?= form_error('email', '<small class="text-danger pl-3">', '</small>'); //menampilkan pemberitahuan error saat tidak memasukkan apapun keform 
+                                        ?>
                                     </div>
                                     <div class="form-group">
                                         <input type="password" class="form-control form-control-user" id="password" name="password" placeholder="Password">
-                                        <?= form_error('password','<small class="text-danger pl-3">','</small>'); //menampilkan pemberitahuan error saat tidak memasukkan apapun keform ?>
+                                        <?= form_error('password', '<small class="text-danger pl-3">', '</small>'); //menampilkan pemberitahuan error saat tidak memasukkan apapun keform 
+                                        ?>
                                     </div>
                                     <button type="submit" class="btn btn-primary btn-user btn-block">
                                         Login
